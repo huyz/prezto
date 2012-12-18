166cf99f79b59ed7ffd7249e305b237106834b66
huyz <h-git@huyz.us>
2012-12-18 10:52:01 -0800

    Workaround for #347: fixing 'ssh <TAB>'

--- modules/huy-reset/init.zsh
+++ modules/huy-reset/init.zsh
@@ -8,3 +8,6 @@ bindkey -M viins -r "kj"
 # People will do -h if they want to
 alias df='df -k'
 alias du='du -k'
+
+# Workaround for https://github.com/sorin-ionescu/prezto/issues/347
+zstyle ':completion:*:ssh:*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *' users
